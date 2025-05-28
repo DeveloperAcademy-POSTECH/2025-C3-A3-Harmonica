f05393547f1a4032058ce53cd710cafe2a8bf158
Daisy Kim <doyeonyyy@gmail.com>
2025-05-28 16:21:32 +0900

    initial commit

---
+++ Harmonica/Harmonica/HarmonicaApp.swift
@@ -0,0 +1,32 @@
+//
+//  HarmonicaApp.swift
+//  Harmonica
+//
+//  Created by Doyeon Kim on 5/28/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct HarmonicaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
