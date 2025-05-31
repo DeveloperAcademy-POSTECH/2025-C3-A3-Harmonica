4a8d74b238711f7cb077f55aada7324bb0f3a961
Daisy Kim <doyeonyyy@gmail.com>
2025-05-31 17:23:10 +0900

    [feat] SongSearchView UI 요소 추가
    음파(waveform) 애니메이션

--- Harmonica/Harmonica/View/Search/SongSearchView.swift
+++ Harmonica/Harmonica/View/Search/SongSearchView.swift
@@ -9,7 +9,7 @@ struct SongSearchView: View {
                 Button(action: {
                     presentationMode.wrappedValue.dismiss()
                 }) {
-                    // 뒤로가기 버튼 이미지
+                    // [이전(돌아가기) 버튼]
                     ZStack {
                         Rectangle()
                             .foregroundColor(.clear)
@@ -31,38 +31,36 @@ struct SongSearchView: View {
                 Spacer()
             }
             Spacer()
-            // 대충 음악을 듣고 있는 이미지
+            
+            // [waveform 애니메이션 이미지 삽입]
             ZStack{
                 Rectangle()
-                  .foregroundColor(.clear)
-                  .frame(width: 260, height: 260)
-                  .background(Color(red: 0.22, green: 0.22, blue: 0.22))
-                  .cornerRadius(260)
-                  .shadow(color: .black.opacity(0.25), radius: 10, x: 0, y: 0)
-                Text("􀞉")
-                  .font(
-                    Font.custom("SF Pro", size: 200)
-                      .weight(.medium)
-                  )
-                  .multilineTextAlignment(.center)
-                  .foregroundColor(Color(red: 0.92, green: 0.9, blue: 0.88))
-                  .symbolEffect(.breathe)
+                    .foregroundColor(.clear)
+                    .frame(width: 260, height: 260)
+                    .background(Color(red: 0.22, green: 0.22, blue: 0.22))
+                    .cornerRadius(260)
+                    .shadow(color: .black.opacity(0.25), radius: 10, x: 0, y: 0)
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 238, height: 238)
+                    .background(Color(red: 0.92, green: 0.9, blue: 0.88))
+                    .cornerRadius(260)
+                    .shadow(color: .black.opacity(0.25), radius: 10, x: 0, y: 0)
+                Image(systemName: "waveform")
+                    .resizable()
+                    .frame(width: 238, height: 238)
+                    .symbolEffect(.breathe)
             }
+            Spacer()
+            
+            // (아직 미적용)모니카의 요청: 텍스트를 한줄로 - 검색시작 1초 이후 "지금 들려주세요 듣고있어요"
             Text("노래를 들려주세요 ")
-              .font(
-                Font.custom("SF Pro", size: 64)
-                  .weight(.medium)
-              )
-              .multilineTextAlignment(.center)
-              .foregroundColor(Color(red: 0.22, green: 0.22, blue: 0.22))
-              .padding()
-            Text("지금 들려주세요 듣고있어요")
-              .font(
-                Font.custom("SF Pro", size: 48)
-                  .weight(.medium)
-              )
-              .multilineTextAlignment(.center)
-              .foregroundColor(Color(red: 0.22, green: 0.22, blue: 0.22))
+                .font(
+                    Font.custom("SF Pro", size: 64)
+                        .weight(.medium)
+                )
+                .multilineTextAlignment(.center)
+                .foregroundColor(Color(red: 0.22, green: 0.22, blue: 0.22))
             Spacer()
         }
         .navigationBarBackButtonHidden(true) // 기본 뒤로가기 버튼 숨김
