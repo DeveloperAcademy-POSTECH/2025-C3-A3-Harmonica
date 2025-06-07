8ed4ecc835e7a274f1c35082285430471a183434
승찬 <anstmdcks12@naver.com>
2025-06-07 21:01:01 +0900

    feat: 노래 검색을 위한 모델과 로직 추가

---
+++ Harmonica/Harmonica/View/Search/MusicManger.swift
@@ -0,0 +1,63 @@
+import Foundation
+import MusicKit
+import AVFoundation
+
+struct Item: Identifiable, Equatable, Hashable {
+  let id: String
+  let title: String
+  let artist: String
+  let previewURL: URL?
+  let artworkURL: URL?
+  
+  init(
+    id: String,
+    title: String,
+    artist: String,
+    previewURL: URL? = nil,
+    artworkURL: URL? = nil)
+  {
+    self.id = id
+    self.title = title
+    self.artist = artist
+    self.previewURL = previewURL
+    self.artworkURL = artworkURL
+  }
+}
+
+
+final class MusicManager {
+  private var player: AVPlayer?
+}
+
+extension MusicManager {
+  func searchTrack(query: String) async throws -> Item? {
+    let request = MusicCatalogSearchRequest(term: query, types: [Song.self])
+    
+    do {
+      let response = try await request.response()
+      
+      guard let song = response.songs.first else { return nil }
+      
+      return .init(
+        id: song.id.rawValue,
+        title: song.title,
+        artist: song.artistName,
+        previewURL: song.previewAssets?.first?.url,
+        artworkURL: song.artwork?.url(width: 270, height: 270))
+      
+    } catch {
+      throw error
+    }
+  }
+  
+  func playPreview(for url: URL) {
+    player = AVPlayer(url: url)
+    player?.play()
+  }
+  
+  func stopPreview() {
+    player?.pause()
+    player = nil
+  }
+}
+
