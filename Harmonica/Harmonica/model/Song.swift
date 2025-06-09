172f20357f53b0c9f9208c2b4a8f09ef644e883e
Yeon Seonae <ifindary@gmail.com>
2025-06-09 22:07:08 +0900

    feat: Add default song data and SongDataManager

--- Harmonica/Harmonica/model/Song.swift
+++ Harmonica/Harmonica/model/Song.swift
@@ -3,15 +3,13 @@ import SwiftData
 
 @Model
 class SongInfo {
-    @Attribute(.unique) var id: Int // 중복되면 안 되는 값
+    @Attribute(.unique) var id: Int
     var title: String
     var artist: String
     var arFileName: String
     var mrFileName: String
     var lyricsFileName: String
     var bpm: Int
-//    var startTime: Double
-//    @Relationship(deleteRule: .cascade) var segments: [Segment] = [] // 부모(SongInfo)가 삭제되면 연결된 자식(Segment)도 삭제, Segment와 1:N 관계
 
     init(id: Int, title: String, artist: String, arFileName: String, mrFileName: String, lyricsFileName: String, bpm: Int) {
         self.id = id
@@ -24,9 +22,17 @@ class SongInfo {
     }
 }
 
+// MARK: - Preview 및 기본 데이터
 extension SongInfo {
     static var preview: SongInfo {
-        // 실제 id: 1641943955, 1759969510
         SongInfo(id: 1, title: "내 여자 내 남자", artist: "배금성", arFileName: "1_ar.mp3", mrFileName: "1_mr.mp3", lyricsFileName: "1_lyrics", bpm: 100)
+    }
+    
+    // 기본 곡 정보들
+    static var defaultSongs: [SongInfo] {
+        return [
+            SongInfo(id: 1641943955, title: "내 여자 내 남자", artist: "배금성", arFileName: "1_ar.mp3", mrFileName: "1_mr.mp3", lyricsFileName: "1_lyrics", bpm: 100),
+            SongInfo(id: 1759969510, title: "내 여자 내 남자", artist: "배금성", arFileName: "1_ar.mp3", mrFileName: "1_mr.mp3", lyricsFileName: "1_lyrics", bpm: 100)
+        ]
     }
 }
